d38365966ddd9f2b91262b63fea4c8c1e39cb9b6
Saragol Barakchian <saragolbarakchian@wlan-e1428.fem.tu-ilmenau.de>
2024-02-12 15:54:13 +0100

    Initial commit. define skeleton of the game; classes, functions, semi-constant properties of the game.

---
+++ app.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+
+
+class Snake:
+    pass
+
+class Food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collisions():
+    pass
+
+def gam_over():
+    pass
+
+
